10c6c8289d2e2949adf7499662802dd736045bef
Amit Thawait <amit17thawait@gmail.com>
2016-09-16 19:40:30 +0530

    Updated array, slice example

--- gopl_io_ex/ch4/rev/main.go
+++ gopl_io_ex/ch4/rev/main.go
@@ -3,20 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{0, 1, 2, 3, 4, 5}
+	// Array
+	nums := [...]int{0, 1, 2, 3, 4, 5}
 	fmt.Printf("Type of nums : %T\n", nums)
 	fmt.Println("Original Array :", nums)
-	reverse(nums)
+	reverse(nums[:])
 	fmt.Println("Reversed Array :", nums)
-	reverse(nums)
-	reverse(nums[:2])
-	reverse(nums[2:])
-	reverse(nums)
-	fmt.Println("Rotated Array :", nums)
+
+	// Slice
+	a := []int{0, 1, 2, 3, 4, 5}
+	reverse(a[:2])
+	reverse(a[2:])
+	reverse(a)
+	fmt.Println("Rotated Array :", a)
 }
 
-func reverse(nums []int) {
-	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
-		nums[i], nums[j] = nums[j], nums[i]
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
 	}
 }
